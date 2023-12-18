0b4ff46618e5ebff49352dfe85adf0ab3e2cfd8a
MordredApCelwydd <protsik.max@ukr.net>
2023-12-19 02:35:38 +0300

    Lab_45

---
+++ KPZ_Lab45/Api_plus_React/KPZ_React_Lab/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KPZ_React_Lab.Models
+{
+     public class EmployeeModel
+     {
+          public int Id { get; set; }
+
+          public string Name { get; set; }
+          public string Surname { get; set; }
+          public string Position { get; set; }
+
+          public double Salary { get; set; }
+     }
+}
---
+++ KPZ_Lab45/Api_plus_React/KPZ_React_Lab/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace KPZ_React_Lab.ViewModels
+{
+     public class ProjectViewModel
+     {
+          public int Id { get; set; }
+
+          public string Name { get; set; }
+          public string Description { get; set; }
+          public string Status { get; set; }
+
+          public int MemberCount { get; set; }
+
+     }
+}
